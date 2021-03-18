a061773eb10d9c61a9772fe8c86da92c08d56970
Henrique Borges <henriqueborgeshbr@gmail.com>
2021-03-18 04:24:31 -0300

    Split pug templates into layout and content

--- build.js
+++ build.js
@@ -3,9 +3,9 @@ import { readFileSync, writeFileSync } from 'fs'
 import stylus from 'stylus'
 import pug from 'pug'
 
-const stylusEntryPoint = readFileSync('./stylus/global.styl', 'utf-8')
-const styles = stylus(stylusEntryPoint).set('paths', ['stylus']).set('compress', true)
+const stylusEntryPoint = readFileSync('./styles/global.styl', 'utf-8')
+const styles = stylus(stylusEntryPoint).set('paths', ['styles']).set('compress', true)
 
-const compiledHtml = pug.renderFile('./crt.pug', { styles: styles.render() })
+const compiledHtml = pug.renderFile('./pages/content.pug', { styles: styles.render() })
 
 writeFileSync('crt.svg', compiledHtml)
